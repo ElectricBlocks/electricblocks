698065fda0f9bc2ac6646abe3617c3f4d78459f8
oceanwhit <oceanwhit@gmail.com>
2020-10-07 00:27:15 -0700

    Added rudimentary wire block (from simple block)

--- src/main/java/edu/uidaho/electricblocks/RegistryHandler.java
+++ src/main/java/edu/uidaho/electricblocks/RegistryHandler.java
@@ -3,11 +3,11 @@ package edu.uidaho.electricblocks;
 import edu.uidaho.electricblocks.blocks.ExternalGridBlock;
 import edu.uidaho.electricblocks.blocks.LampBlock;
 import edu.uidaho.electricblocks.blocks.PortableGeneratorBlock;
-import edu.uidaho.electricblocks.blocks.SimpleBlock;
+import edu.uidaho.electricblocks.blocks.WireBlock;
 import edu.uidaho.electricblocks.items.ExternalGridItem;
 import edu.uidaho.electricblocks.items.LampItem;
 import edu.uidaho.electricblocks.items.PortableGeneratorItem;
-import edu.uidaho.electricblocks.items.SimpleItem;
+import edu.uidaho.electricblocks.items.WireItem;
 import edu.uidaho.electricblocks.tileentities.ExternalGridTileEntity;
 import edu.uidaho.electricblocks.tileentities.LampTileEntity;
 import net.minecraft.block.Block;
@@ -27,13 +27,13 @@ public class RegistryHandler {
 
     // Blocks
     public static final RegistryObject<Block> PORTABLE_GENERATOR_BLOCK = BLOCKS.register("portablegenerator_block", PortableGeneratorBlock::new);
-    public static final RegistryObject<Block> SIMPLE_BLOCK = BLOCKS.register("simple_block", SimpleBlock::new);
+    public static final RegistryObject<Block> WIRE_BLOCK = BLOCKS.register("wire_block", WireBlock::new);
     public static final RegistryObject<Block> EXTERNAL_GRID_BLOCK = BLOCKS.register("externalgrid_block", ExternalGridBlock::new);
     public static final RegistryObject<Block> LAMP_BLOCK = BLOCKS.register("lamp_block", LampBlock::new);
 
     // Items
     public static final RegistryObject<Item> PORTABLE_GENERATOR_ITEM = ITEMS.register("portablegenerator_item", PortableGeneratorItem::new);
-    public static final RegistryObject<Item> SIMPLE_ITEM = ITEMS.register("simple_item", SimpleItem::new);
+    public static final RegistryObject<Item> WIRE_ITEM = ITEMS.register("wire_item", WireItem::new);
     public static final RegistryObject<Item> EXTERNAL_GRID_ITEM = ITEMS.register("externalgrid_item", ExternalGridItem::new);
     public static final RegistryObject<Item> LAMP_ITEM = ITEMS.register("lamp_item", LampItem::new);
 
---
+++ src/main/java/edu/uidaho/electricblocks/blocks/WireBlock.java
@@ -0,0 +1,17 @@
+package edu.uidaho.electricblocks.blocks;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.material.Material;
+
+public class WireBlock extends Block {
+
+    public WireBlock() {
+        super(Block.Properties.create(Material.EARTH)
+                .hardnessAndResistance(0f)
+                .sound(SoundType.ANVIL)
+                .slipperiness(0.6f)
+                .harvestLevel(2)
+        );
+    }
+}
