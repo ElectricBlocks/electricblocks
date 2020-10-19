8b0a0727eb3fcf65c291e7481918dc9b315dff9c
Zach Sugano <zachoooo@gmail.com>
2020-10-18 18:56:02 -0700

    feat: implemented connecting blocks

--- src/main/java/edu/uidaho/electricblocks/simulation/SimulationNetwork.java
+++ src/main/java/edu/uidaho/electricblocks/simulation/SimulationNetwork.java
@@ -1,11 +1,17 @@
 package edu.uidaho.electricblocks.simulation;
 
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 import com.google.gson.JsonObject;
 
 import edu.uidaho.electricblocks.ElectricBlocksMod;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
 
 public class SimulationNetwork {
 
@@ -50,7 +56,28 @@ public class SimulationNetwork {
     }
 
     public void addConnectedBlocks() {
+        HashSet<BlockPos> checked = new HashSet<>();
+        Queue<BlockPos> unchecked = new LinkedList<>();
+        World world = simTileEntities.get(0).getWorld();
+        unchecked.add(simTileEntities.get(0).getPos());
+        
+        while (!unchecked.isEmpty()) {
+            BlockPos bPos = unchecked.remove();
+            if (checked.contains(bPos)) continue; // Skip if this block was already checked
 
+            TileEntity te = world.getChunk(bPos).getTileEntity(bPos); // Might not be thread safe
+            if (te != null && te instanceof SimulationTileEntity) {
+                SimulationTileEntity ste = (SimulationTileEntity) te;
+                simTileEntities.add(ste);
+                unchecked.add(bPos.up());
+                unchecked.add(bPos.down());
+                unchecked.add(bPos.north());
+                unchecked.add(bPos.south());
+                unchecked.add(bPos.east());
+                unchecked.add(bPos.west());
+            }
+            checked.add(bPos);
+        }
     }
 
 }
