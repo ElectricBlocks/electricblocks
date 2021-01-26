b995ed5fb4af032bd28d154075ffe0c7e0b1c0e8
Zach Sugano <zachoooo@gmail.com>
2021-01-25 21:39:59 -0800

    feat: reorganized loadscreen gui
    see issue #8

--- src/main/java/edu/uidaho/electricblocks/guis/LoadScreen.java
+++ src/main/java/edu/uidaho/electricblocks/guis/LoadScreen.java
@@ -1,40 +1,43 @@
 package edu.uidaho.electricblocks.guis;
 
-import edu.uidaho.electricblocks.ElectricBlocksMod;
+import org.lwjgl.glfw.GLFW;
+
 import edu.uidaho.electricblocks.electric.Watt;
 import edu.uidaho.electricblocks.tileentities.LoadTileEntity;
 import edu.uidaho.electricblocks.utils.PlayerUtils;
-import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.widget.TextFieldWidget;
 import net.minecraft.client.gui.widget.button.Button;
-import net.minecraft.client.gui.widget.list.OptionsRowList;
 import net.minecraft.client.resources.I18n;
-import net.minecraft.client.settings.BooleanOption;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.util.text.TranslationTextComponent;
 
 public class LoadScreen extends Screen {
-    /** Width of a button */
+    // Layout constants 
+    // Width of a button
     private static final int BUTTON_WIDTH = 200;
-    /** Height of a button */
+    // Height of a button
     private static final int BUTTON_HEIGHT = 20;
-    /** Width of text input */
-    private static final int TEXT_INPUT_WIDTH = 140;
-    /** Height of text input */
+    // Width of text input
+    private static final int TEXT_INPUT_WIDTH = 100;
+    // Height of text input
     private static final int TEXT_INPUT_HEIGHT = 20;
-    /** Distance from bottom of the screen to the "Done" button's top */
+    // Distance from bottom of the screen to the "Done" button's top
     private static final int DONE_BUTTON_TOP_OFFSET = 26;
-    /** Distance from top of the screen to this GUI's title */
+    // Distance from top of the screen to this GUI's title
     private static final int TITLE_HEIGHT = 8;
-    /** Width of max power label */
+    // Width of max power label
 
+    // Layout elements
+    private Button doneButton;
     private TextFieldWidget textFieldMaxPower;
+    private TextFieldWidget textFieldResultPower;
 
+    // Info needed to preload the form with data
+    private boolean changed = false;
     private LoadTileEntity loadTileEntity;
     private PlayerEntity player;
     private boolean inService;
-    private Watt resultPower;
 
     public LoadScreen(LoadTileEntity loadTileEntity, PlayerEntity player) {
         super(new TranslationTextComponent("gui.electricblocks.loadscreen"));
@@ -45,26 +48,30 @@ public class LoadScreen extends Screen {
     @Override
     protected void init() {
         inService = loadTileEntity.isInService();
-        resultPower = loadTileEntity.getResultPower();
-        textFieldMaxPower = new TextFieldWidget(font, (this.width - TEXT_INPUT_WIDTH) / 2 + (BUTTON_WIDTH - TEXT_INPUT_WIDTH) / 2, 50, TEXT_INPUT_WIDTH, TEXT_INPUT_HEIGHT, loadTileEntity.getMaxPower().toString());
+        textFieldMaxPower = new TextFieldWidget(font, (this.width - TEXT_INPUT_WIDTH) / 2 + (BUTTON_WIDTH - TEXT_INPUT_WIDTH) / 2, 25, TEXT_INPUT_WIDTH, TEXT_INPUT_HEIGHT, loadTileEntity.getMaxPower().toString());
         textFieldMaxPower.setText(String.format("%f", loadTileEntity.getMaxPower().getMegaWatts()));
         textFieldMaxPower.setFocused2(true);
         textFieldMaxPower.setVisible(true);
 
+        textFieldResultPower = new TextFieldWidget(font, (this.width - TEXT_INPUT_WIDTH) / 2 + (BUTTON_WIDTH - TEXT_INPUT_WIDTH) / 2, 60, TEXT_INPUT_WIDTH, TEXT_INPUT_HEIGHT, loadTileEntity.getMaxPower().toString());
+        textFieldResultPower.setText(String.format("%f", loadTileEntity.getResultPower().getMegaWatts()));
+        textFieldResultPower.setVisible(true);
+
         // Add the "Done" button
-        this.addButton(new Button(
-                (this.width - BUTTON_WIDTH) / 2,
-                this.height - DONE_BUTTON_TOP_OFFSET,
-                BUTTON_WIDTH, BUTTON_HEIGHT,
-                // Text shown on the button
-                I18n.format("gui.done"),
-                // Action performed when the button is pressed
-                button -> onClose()
-        ));
+        doneButton = new Button(
+            (this.width - BUTTON_WIDTH) / 2,
+            this.height - DONE_BUTTON_TOP_OFFSET,
+            BUTTON_WIDTH, BUTTON_HEIGHT,
+            // Text shown on the button
+            I18n.format("gui.done"),
+            // Action performed when the button is pressed
+            button -> onClose()
+        );
+        this.addButton(doneButton);
 
         this.addButton(new Button(
             (this.width - BUTTON_WIDTH) / 2,
-            75,
+            this.height - 2 * DONE_BUTTON_TOP_OFFSET,
             BUTTON_WIDTH, BUTTON_HEIGHT,
             inService ? "In Service: true" : "In Service: false",
             button -> {
@@ -75,6 +82,7 @@ public class LoadScreen extends Screen {
                     button.setMessage("In Service: true");
                     inService = true;
                 }
+                onChange();
             }));
     }
 
@@ -85,12 +93,16 @@ public class LoadScreen extends Screen {
         // Draw the title
         this.drawCenteredString(this.font, this.title.getFormattedText(),
                 this.width / 2, TITLE_HEIGHT, 0xFFFFFF);
-        // Draw max power label
-        this.drawString(this.font, "Max Power", (this.width - TEXT_INPUT_WIDTH) / 2 - (BUTTON_WIDTH - TEXT_INPUT_WIDTH) / 2, 50 + (this.font.FONT_HEIGHT / 2), 0xFFFFFF);
+        // Draw property labels
+        this.drawString(this.font, "Max Power", (this.width - TEXT_INPUT_WIDTH) / 2 - (BUTTON_WIDTH - TEXT_INPUT_WIDTH) / 2, 25 + (this.font.FONT_HEIGHT / 2), 0xFFFFFF);
+        this.drawString(this.font, "Result Power", (this.width - TEXT_INPUT_WIDTH) / 2 - (BUTTON_WIDTH - TEXT_INPUT_WIDTH) / 2, 60 + (this.font.FONT_HEIGHT / 2), 0xFFFFFF);
         // Draw mw label
-        this.drawString(this.font, "MW", (this.width / 2) + (TEXT_INPUT_WIDTH / 2) + 40, 50 + (this.font.FONT_HEIGHT / 2), 0xFFFFFF);
-        this.drawString(this.font, "Result Power: " + String.format("%f", resultPower.getMegaWatts()) + " MW", (this.width / 2) - 20, 100, 0xFFFFFF);
+        this.drawString(this.font, "MW", (this.width / 2) + (TEXT_INPUT_WIDTH / 2) + 55, 25 + (this.font.FONT_HEIGHT / 2), 0xFFFFFF);
+        this.drawString(this.font, "MW", (this.width / 2) + (TEXT_INPUT_WIDTH / 2) + 55, 60 + (this.font.FONT_HEIGHT / 2), 0xFFFFFF);
+        // Draw separator
+        this.drawCenteredString(this.font, "- - - - - - - - - - - - - - - - - - - -", this.width / 2, 45 + (this.font.FONT_HEIGHT / 2), 0xFFFFFF);
         textFieldMaxPower.render(mouseX, mouseY, partialTicks);
+        textFieldResultPower.render(mouseX, mouseY, partialTicks);
         // Call the super class' method to complete rendering
         super.render(mouseX, mouseY, partialTicks);
     }
@@ -99,6 +111,7 @@ public class LoadScreen extends Screen {
     public boolean charTyped(char p_charTyped_1_, int p_charTyped_2_) {
         if (textFieldMaxPower.isFocused()) {
             textFieldMaxPower.charTyped(p_charTyped_1_, p_charTyped_2_);
+            onChange();
         }
         return super.charTyped(p_charTyped_1_, p_charTyped_2_);
     }
@@ -107,6 +120,9 @@ public class LoadScreen extends Screen {
     public boolean keyPressed(int p_keyPressed_1_, int p_keyPressed_2_, int p_keyPressed_3_) {
         if (textFieldMaxPower.isFocused()) {
             textFieldMaxPower.keyPressed(p_keyPressed_1_, p_keyPressed_2_, p_keyPressed_3_);
+            if (p_keyPressed_1_ == GLFW.GLFW_KEY_BACKSPACE) {
+                onChange();
+            }
         }
         return super.keyPressed(p_keyPressed_1_, p_keyPressed_2_, p_keyPressed_3_);
     }
@@ -126,23 +142,33 @@ public class LoadScreen extends Screen {
     
     @Override
     public void onClose() {
-        boolean shouldUpdate = true;
-        double maxPower = 0;
-        try {
-            maxPower = Double.parseDouble(textFieldMaxPower.getText());
-        } catch (NumberFormatException e) {
-            shouldUpdate = false;
-            PlayerUtils.error(player, "gui.electricblocks.err_invalid_number");
-        }
+        if (changed) {
+            boolean shouldUpdate = true;
+            double maxPower = 0;
+            try {
+                maxPower = Double.parseDouble(textFieldMaxPower.getText());
+            } catch (NumberFormatException e) {
+                shouldUpdate = false;
+                PlayerUtils.error(player, "gui.electricblocks.err_invalid_number");
+            }
 
-        if (shouldUpdate) {
-            loadTileEntity.setInService(inService);
-            loadTileEntity.setMaxPower(new Watt(maxPower * 1000000));
-            loadTileEntity.notifyUpdate();
+            if (shouldUpdate) {
+                PlayerUtils.sendMessage(player, "command.electricblocks.viewmodify.submit");
+                loadTileEntity.setInService(inService);
+                loadTileEntity.setMaxPower(new Watt(maxPower * 1000000));
+                loadTileEntity.notifyUpdate();
 
-            loadTileEntity.requestSimulation(player);
+                loadTileEntity.requestSimulation(player);
+            }
         }
         super.onClose();
+    }
+
+    public void onChange() {
+        if (!changed) {
+            changed = true;
+            doneButton.setMessage("* " + I18n.format("gui.done") + " *");
+        }
     }
 
 }
