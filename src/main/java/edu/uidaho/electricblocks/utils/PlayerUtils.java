92eedc35f94a5f029ee4e4f345b844d03236dde9
Zach Sugano <zachoooo@gmail.com>
2021-02-04 16:34:07 -0800

    feat: optimized and documented PlayerUtils.java
    There were some unnecessary checks going on in this class that could be safely removed. I also added documentation as well as send raw message in case we need that

--- src/main/java/edu/uidaho/electricblocks/utils/PlayerUtils.java
+++ src/main/java/edu/uidaho/electricblocks/utils/PlayerUtils.java
@@ -1,24 +1,27 @@
 package edu.uidaho.electricblocks.utils;
 
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.util.text.TextFormatting;
-import net.minecraft.util.text.TranslationTextComponent;
+import net.minecraft.util.text.*;
 
 public class PlayerUtils {
 
+    /**
+     * Sends a localized messaged to the player without any formatting.
+     * @param player The player to send the message to
+     * @param message The translation key for the message you want to send
+     * @param args Optional positional arguments used in string formatting
+     */
     public static void sendMessage(PlayerEntity player, String message, Object... args) {
-        if (player.getEntityWorld().isRemote()) {
-            return;
-        }
-
         player.sendMessage(new TranslationTextComponent(message, args));
     }
 
+    /**
+     * Sends a localized message to the player with a red [WARNING] tag in front of it.
+     * @param player The player to send the warning to
+     * @param warning The translation key for the warning you want to send
+     * @param args Optional positional arguments used in string formatting
+     */
     public static void warn(PlayerEntity player, String warning, Object... args) {
-        if (player.getEntityWorld().isRemote()) {
-            return;
-        }
-        
         TranslationTextComponent parent = new TranslationTextComponent("command.electricblocks.warn");
         parent.applyTextStyle(TextFormatting.RED);
         parent.appendText(": ");
@@ -30,11 +33,13 @@ public class PlayerUtils {
         player.sendMessage(parent);
     }
 
+    /**
+     * Sends a localized message to the player with a dark red [ERROR] tag in front of it.
+     * @param player The player to send the error to
+     * @param err The translation key for the error you want to send
+     * @param args Optional positional arguments used in string formatting
+     */
     public static void error(PlayerEntity player, String err, Object... args) {
-        if (player.getEntityWorld().isRemote()) {
-            return;
-        }
-
         TranslationTextComponent parent = new TranslationTextComponent("command.electricblocks.error");
         parent.applyTextStyle(TextFormatting.DARK_RED);
         parent.appendText(": ");
@@ -44,6 +49,17 @@ public class PlayerUtils {
         parent.appendSibling(sibling);
 
         player.sendMessage(parent);
+    }
+
+    /**
+     * Sends a raw unformatted and non-localized message to the player. This is not recommended, since localization is
+     * highly recommended, but I figured this should still be in here.
+     * @param player The player to send the raw message to
+     * @param message The messsage itself
+     */
+    @Deprecated
+    public static void sendRaw(PlayerEntity player, String message) {
+        player.sendMessage(new StringTextComponent(message));
     }
     
 }
