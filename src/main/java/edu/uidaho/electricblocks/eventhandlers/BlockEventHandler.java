8530407d9f2511374cde4f959630e0e249a9ce6f
Zach Sugano <zachoooo@gmail.com>
2021-04-17 13:22:02 -0700

    doc: commented BlockEventHandler

--- src/main/java/edu/uidaho/electricblocks/eventhandlers/BlockEventHandler.java
+++ src/main/java/edu/uidaho/electricblocks/eventhandlers/BlockEventHandler.java
@@ -18,26 +18,37 @@ import net.minecraftforge.event.world.BlockEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
+/**
+ * This class holds methods called by Forge API when block based events are triggered. This is mostly used to make the
+ * multimeter item function correctly.
+ */
 @Mod.EventBusSubscriber
 public class BlockEventHandler {
-    
+
+    /**
+     * This function is called by Forge API when a player left clicks on a block. This is used to detect when a player
+     * left clicks on a block with the multimeter in hand so the toggle/update operation can be performed.
+     * @param event The event object that triggered this function call
+     */
     @SubscribeEvent
     public void blockLeftClicked(PlayerInteractEvent.LeftClickBlock event) {
         World world = event.getWorld();
         PlayerEntity player = event.getPlayer();
 
         Item item = player.getHeldItem(Hand.MAIN_HAND).getItem();
-        if (item == RegistryHandler.MULTIMETER_ITEM.get()) {
+        if (item == RegistryHandler.MULTIMETER_ITEM.get()) { // Only process if player is holding multimeter
             BlockState blockState = world.getBlockState(event.getPos());
             Block block = blockState.getBlock();
 
             IMultimeter multimeter;
             TileEntity te;
+            // Both blocks and tile entities can implement the IMultimeter interface
             if (block instanceof IMultimeter) {
                 multimeter = (IMultimeter) block;
             } else if ((te = world.getTileEntity(event.getPos())) instanceof IMultimeter) {
                 multimeter = (IMultimeter) te;
             } else {
+                // Player clicked on a block that doesn't implement interface
                 PlayerUtils.error(event.getPlayer(), "command.electricblocks.updatetoggle.err_invalid_block");
                 return;
             }
@@ -47,23 +58,30 @@ public class BlockEventHandler {
         }
     }
 
+    /**
+     * This function is called by the Forge API when a player right clicks on a block. This is used to detect when the
+     * player right clicks on a block with the multimeter item in hand so the view/modify operation can be performed.
+     * @param event The event object that triggered this function call.
+     */
     @SubscribeEvent
     public void blockRightClicked(PlayerInteractEvent.RightClickBlock event) {
         World world = event.getWorld();
         PlayerEntity player = event.getPlayer();
 
         Item item = player.getHeldItem(Hand.MAIN_HAND).getItem();
-        if (item == RegistryHandler.MULTIMETER_ITEM.get()) {
+        if (item == RegistryHandler.MULTIMETER_ITEM.get()) { // Only process if player is holding multimeter
             BlockState blockState = world.getBlockState(event.getPos());
             Block block = blockState.getBlock();
 
             IMultimeter multimeter;
             TileEntity te;
+            // Both blocks and tile entities can implement the IMultimeter interface
             if (block instanceof IMultimeter) {
                 multimeter = (IMultimeter) block;
             } else if ((te = world.getTileEntity(event.getPos())) instanceof IMultimeter) {
                 multimeter = (IMultimeter) te;
             } else {
+                // Player clicked on a block that doesn't implement interface
                 PlayerUtils.error(event.getPlayer(), "command.electricblocks.viewmodify.err_invalid_block");
                 return;
             }
@@ -72,18 +90,23 @@ public class BlockEventHandler {
         }
     }
 
+    /**
+     * This funciton is called by the Forge API when a player breaks a block. This is used to prevent the update/toggle
+     * action from breaking blocks when players left click on an electric block in game.
+     * @param event The event object that triggered this function call
+     */
     @SubscribeEvent
     public void blockBroken(BlockEvent.BreakEvent event) {
         PlayerEntity player = event.getPlayer();
         Item item = player.getHeldItem(Hand.MAIN_HAND).getItem();
         BlockPos pos = event.getPos();
 
-        if (item == RegistryHandler.MULTIMETER_ITEM.get()) {
+        if (item == RegistryHandler.MULTIMETER_ITEM.get()) { // Cancel event if player is holding multimeter
             event.setCanceled(true);
             return;
         }
 
-        if (ElectricBlocksConfig.getUpdateOnBlockBreak()) {
+        if (ElectricBlocksConfig.getUpdateOnBlockBreak()) { // Request simulation if configured to do so
             TileEntity te;
             if ((te = event.getWorld().getTileEntity(pos)) instanceof SimulationTileEntity) {
                 PlayerUtils.warn(player, "command.electricblocks.block_broken");
