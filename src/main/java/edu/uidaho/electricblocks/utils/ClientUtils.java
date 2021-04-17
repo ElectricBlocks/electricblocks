1afa45bdb35a892915c011e5584088a9eaa3e3f1
Zach Sugano <zachoooo@gmail.com>
2021-04-17 16:01:34 -0700

    doc: commented ClientUtils

--- src/main/java/edu/uidaho/electricblocks/utils/ClientUtils.java
+++ src/main/java/edu/uidaho/electricblocks/utils/ClientUtils.java
@@ -16,12 +16,20 @@ import net.minecraftforge.fml.ModLoadingContext;
  */
 public class ClientUtils {
 
-    private ClientUtils() {}
+    private ClientUtils() {} // Class is not instantiable and just holds static methods
 
+    /**
+     * Opens a new GUI screen for player to interact with a STE
+     * @param simulationTileEntity The STE to display
+     * @param playerEntity The player to display the STEScreen to
+     */
     public static void openSTEScreen(SimulationTileEntity simulationTileEntity, PlayerEntity playerEntity) {
         Minecraft.getInstance().displayGuiScreen(new STEScreen(simulationTileEntity, playerEntity));
     }
 
+    /**
+     * Registers client-side config screen with Forge so it can be opened from the mods menu
+     */
     public static void registerConfigScreen() {
         ModLoadingContext.get().registerExtensionPoint(ExtensionPoint.CONFIGGUIFACTORY,
                 () -> (mc, screen) -> new ConfigScreen());
